6fd86a9a0af3b8cd0f402a0d6450af1f0ad4692f
alekhyashekhar <alekhyashekhar05@gmail.com>
2024-10-03 14:15:08 +0530

    Create collatz.py

---
+++ collatz.py
@@ -0,0 +1,51 @@
+'''
+The rules for generating Collatz Sequence are:
+If n is even:   n = n / 2
+If n is odd:    n = 3n + 1
+For example, if the starting number is 5 the sequence is:
+5 -> 16 -> 8 -> 4 -> 2 -> 1
+It has been proved for almost all integers, the repeated application of the above rule will result in a sequence that ends at 1.
+Input format:
+The input containing an integer 'n' which denotes the given number
+Output format:
+Print the numbers in the sequence and also print the number of times the rule has to be applied in order to reach 1.
+Refer the sample output for formatting.
+Sample Input:
+5
+Sample Output:
+5
+16
+8
+4
+2
+1
+5
+'''
+
+
+n = int(input())  # Input the starting number
+
+# Initialize a counter for the number of steps
+count = 0
+
+# Store the sequence in a list for printing later
+collatz_sequence = []
+
+# Generate the Collatz sequence
+while n != 1:
+    collatz_sequence.append(n)  # Add the current number to the sequence
+    if n % 2 == 0:  # n is even
+        n = n // 2
+    else:  # n is odd
+        n = 3 * n + 1
+    count += 1  # Increment the step count
+
+# Append the final number (1) to the sequence
+collatz_sequence.append(1)
+
+# Print the sequence
+for number in collatz_sequence:
+    print(number)
+
+# Print the number of steps
+print(count)
